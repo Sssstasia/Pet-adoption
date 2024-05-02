1a1f2a753983bd11df9dfc48742753534c1e0736
Stasia <aid.alakova@gmail.com>
2024-05-02 15:44:17 +0100

    finishedt

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ start()
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherData = await weatherPromise.json()
-    const miamiTemperature = (weatherData.properties.periods[0].temperature)
+    const miamiTemperature = weatherData.properties.periods[0].temperature
 
-    console.log(miamiTemperature)
+    document.querySelector("#temperature-output").textContent = miamiTemperature
 }
 
