bc20677a36472531a61eadb7254251be8d38a62e
Stasia <aid.alakova@gmail.com>
2024-05-03 16:15:36 +0100

    broken cards

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-start()
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+
 
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
@@ -7,4 +10,19 @@ async function start() {
 
     document.querySelector("#temperature-output").textContent = miamiTemperature
 }
+
+start()
+
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        const clone = template.content.cloneNode(true)
+        wrapper.appendChild(clone)
+    })
+    document.querySelector(".list-of-pets").appendChild(wrapper)
+}
+
+
+petsArea()
 
